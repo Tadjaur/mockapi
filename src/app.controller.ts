4d890d687e8ea28eb0d3ae5d2236bbd672bdf14c
Tadjaur <wk.taur@gmail.com>
2023-04-10 15:38:35 +0100

    create first config file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import type { Response } from 'express';
 import { AppService } from './app.service';
+import type {AxiosError} from 'axios';
 
+const gitApiHost = 'https://api.github.com';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService,
+  ) {}
 
-  @Get(':gitUser/:gitRepository')
-  getRepoPage(): string {
-    return this.appService.getHello();
+  @Get(':githubId/:repository/*')
+  async getRepoPage(@Param() params, @Res() res: Response): Promise<void> {
+    // const config
+    this.httpService.get(
+      `${gitApiHost}/repos/${params.githubId}/${params.repository}/contents/.mockapi`,
+      {
+        headers: {
+          "Content-Type": "application/vnd.github.raw"
+        }
+      },
+    ).subscribe({
+      next(data){
+        res.status(200).send(data);
+      },
+      error(err:AxiosError){
+        res.status(err.status ?? 400).json({message: err.message, data: err.toJSON?.call({})})
+      }
+    });
   }
-  
+
   @Get(':gitUser/:gitRepository')
   getRepoConfig(): string {
     return this.appService.getHello();
---
+++ src/services/configuration.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+Injectable();
+class ApiConfig {
+  loadConfig(fileContain: string): Record<string, unknown> {
+    return {};
+  }
+}
