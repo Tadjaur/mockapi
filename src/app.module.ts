de3907ed124e832292553500e5716618e5c90de8
Tadjaur <wk.taur@gmail.com>
2023-04-10 09:23:50 +0100

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoggerMiddleware } from './middleware/access_log';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*');
+  }
+}
